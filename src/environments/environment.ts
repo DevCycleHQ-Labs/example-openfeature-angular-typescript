7162b44290ca6e8a88581d9e0471ad58a15b9804
Jonathan Norris <jonathan@taplytics.com>
2025-02-18 12:26:29 -0500

    feat: update example app to load SDK key from .env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,12 @@
+declare global {
+  interface Window {
+    devcycleEnv?: {
+      DEVCYCLE_CLIENT_SDK_KEY: string;
+    };
+  }
+}
+
 export const environment = {
-  DEVCYCLE_CLIENT_SDK_KEY: "<DEVCYCLE_CLIENT_SDK_KEY>",
+  DEVCYCLE_CLIENT_SDK_KEY:
+    window.devcycleEnv?.DEVCYCLE_CLIENT_SDK_KEY || "<DEVCYCLE_CLIENT_SDK_KEY>",
 };
