ca47c09947c7a24d2ac033420001c93be85f1056
Jonathan Norris <jonathan@taplytics.com>
2025-02-18 13:49:48 -0500

    fix: loading variables from .env file

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
-require('dotenv').config();
+const dotenv = require('dotenv');
+
+// Load .env file from project root
+const env = dotenv.parse(fs.readFileSync('.env'));
 
 const envScript = `
 window.devcycleEnv = {
-  DEVCYCLE_CLIENT_SDK_KEY: "${process.env.DEVCYCLE_CLIENT_SDK_KEY}"
+  DEVCYCLE_CLIENT_SDK_KEY: "${env.DEVCYCLE_CLIENT_SDK_KEY || process.env.DEVCYCLE_CLIENT_SDK_KEY}"
 };
 `;
 
-fs.writeFileSync('src/assets/env.js', envScript); 
+fs.writeFileSync('src/assets/env.js', envScript);
